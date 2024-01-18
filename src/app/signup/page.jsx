afe6d8e7a60384e94f40be25c63cf62835fdb7dd
Tashiev Emir <tashievemir01@gmail.com>
2024-01-18 19:44:48 +0600

    added register request

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -11,20 +11,32 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import $api from '../../utils/api'
+import { useRouter } from 'next/navigation';
 
 const defaultTheme = createTheme();
 
 const SignUp = () => {
+  const router = useRouter()
+
     const handleSubmit = async (event) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
 
-        const response = await axios.get();
-
-        console.log({
-          firstName: data.get('firstName'),
-          lastName: data.get('lastName'),
+        const response = await $api.post('/Account/Register',{
+          LastName: data.get('lastName'),
+          FirstName: data.get('firstName'),
+          Email: data.get('email'),
+          Password: data.get('password'),
+          RepeatedPassword: data.get('repeatPassword'),
+          Phone: data.get('telefon'),
+          Faculty: data.get('faculty'),
+          Department: data.get('department')
         });
+        if(response.status==200){
+          router.push('/sigin');
+        }
+
       };
 
   return (
@@ -47,89 +59,89 @@ const SignUp = () => {
           </Typography>
           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="Атыныз"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Фамилияныз"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12} >
-                <TextField
-                  required
-                  fullWidth
-                  id="faculty"
-                  label="Факультет"
-                  name="faculty"
-                  autoComplete="faculty-name"
-                />
-              </Grid>
-              <Grid item xs={12} >
-                <TextField
-                  required
-                  fullWidth
-                  id="department"
-                  label="Бөлүм"
-                  name="department"
-                  autoComplete="department-name"
-                />
-              </Grid>
-              <Grid item xs={12} >
-                <TextField
-                  required
-                  fullWidth
-                  id="telefon"
-                  label="Телефон"
-                  name="telefon"
-                  autoComplete="telefon-name"
-                />
-              </Grid>
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Манас почта"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Пароль"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  name="repeatPassword"
-                  label="Парольду кайталаныз"
-                  type="repeatPassword"
-                  id="repeatPassword"
-                  autoComplete="repeatPassword"
-                />
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="firstName"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="Атыныз"
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Фамилияныз"
+                    name="lastName"
+                    autoComplete="family-name"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="faculty"
+                    label="Факультет"
+                    name="faculty"
+                    autoComplete="faculty-name"
+                  />
+                </Grid>
+                <Grid item xs={12} >
+                  <TextField
+                    required
+                    fullWidth
+                    id="department"
+                    label="Бөлүм"
+                    name="department"
+                    autoComplete="department-name"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="telefon"
+                    label="Телефон"
+                    name="telefon"
+                    autoComplete="telefon-name"
+                  />
+                </Grid>
+                <Grid item xs={12} >
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Манас почта"
+                    name="email"
+                    autoComplete="email"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Пароль"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="repeatPassword"
+                    label="Парольду кайталаныз"
+                    type="password"
+                    id="repeatPassword"
+                    autoComplete="repeatPassword"
+                  />
+                </Grid>
               </Grid>
             <Button
               type="submit"
